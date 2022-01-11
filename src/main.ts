15b525f02c6ebc3c651da31f348b31dd617db44c
Omid Nasiri <omid_nasiriii@outlook.com>
2022-01-11 23:41:45 +0330

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: '*',
+    credentials: true
+  });
   await app.listen(4000);
 }
 bootstrap();
